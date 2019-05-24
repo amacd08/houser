3bf7bd9ca43562980e8dde1156c8dc6e152519b7
amacd08 <amacd08@gmail.com>
2019-05-24 11:07:24 -0600

    dotenv

--- server/index.js
+++ server/index.js
@@ -1,12 +1,15 @@
 const express = require('express')
 require('dotenv').config()
 const {SERVER_PORT, CONNECTION_STRING} = process.env
-
+const massive = require('massive')
 const app = express()
 app.use(express.json())
 
 
-
+massive(CONNECTION_STRING).then(db => {
+    app.set('db',db)
+    console.log('db connection established')
+})
 
 
 app.listen(SERVER_PORT,() => console.log(`Listening on ${SERVER_PORT}`))
